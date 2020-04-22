53219cd80a494e1e6a833903c92318b6fa8b46aa
Wu Xing <1205118129@qq.com>
2020-04-22 21:09:21 +0800

    mi-mall首页

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>footer</div>
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="">顾长安主页<span>|</span></a>
+            <a href="">顾长安博客<span>|</span></a>
+            <a href="">关于小米<span>|</span></a>
+            <a href="">与我有关<span>|</span></a>
+            <a href="">线下门店<span>|</span></a>
+        </div>
+        <div class="copright"> Select Location © mi.com 京ICP证110507号 京ICP备10046444号 京公网安备11010802020134号 京网文[2020]0276-042号</div>
+    </div>
 </template>
 
 <script>
@@ -7,3 +20,36 @@
         name:'nav-footer',
     }
 </script>
+
+<style lang="scss">
+    .footer{
+        height:234px;
+        border-top:4px solid #FF6600;
+        background-color:#333333;
+        color:#999999;
+        font-size:16px;
+        text-align:center;
+        .footer-logo{
+            margin-top:46px;
+            margin-bottom:31px;
+            img{
+                 width:53px;
+                 height:36px;
+                 margin-bottom:13px;
+            }
+             
+        }
+        .footer-link{
+            a{
+                color:#999999; 
+                display:inline-block;
+                span{
+                    margin-left:4px;
+                    margin-right:4px;
+                }
+            }
+            margin-bottom:13px; 
+        }
+        
+    }
+</style>
